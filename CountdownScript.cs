c8d1aa8cbbc7aa5928a0f187e9d94eee4e36965b
Adam Rushton <adam.j.rushton@gmail.com>
2018-04-24 17:06:35 +0100

    Update

---
+++ CountdownScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class CountdownScript : UnityEngine.MonoBehaviour
+{
+    public UnityEngine.UI.Text Countdown; // Text component on interface
+
+    private float startTime = 5.5f; // Start counting down from
+    private float timeRemaining;    // Calculate time remaining
+    private float endTime = 0.5f;   // Time to stop counting down at
+
+	// Use this for initialization
+	void Start()
+    {
+        timeRemaining = startTime;
+        Countdown = Countdown.GetComponent<UnityEngine.UI.Text>();
+	}
+	
+    // Countdown system that runs in the game loop
+    // Using delta time, updating the text component on screen with time until start
+    private void CountdownSystem()
+    {
+        if (timeRemaining > endTime)
+        {
+            timeRemaining -= UnityEngine.Time.deltaTime;
+            Countdown.text = System.Math.Round(timeRemaining).ToString();
+        }
+        else
+        {
+            Countdown.text = "Go!";
+        }
+    }
+	// Update is called once per frame
+	void Update()
+    {
+        CountdownSystem();
+	}
+}
---
+++ New Highscores system/testscore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class testscore : MonoBehaviour
+{
+
+    int score = 50;
+    int gameModeNumber = 3; // Game mode 3 has finished
+	// Use this for initialization
+	void Start()
+    {
+        PlayerPrefs.SetInt("GameModeNumber", gameModeNumber); // Store game mode number in playerprefs
+        PlayerPrefs.SetInt("FinalScore"+gameModeNumber, score);
+	}
+	
+    public void LoadHighscoresScene()
+    {
+        try
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("5scores"); // Will change to +gameModeNumber
+        }
+        catch (System.Exception e)
+        {
+            UnityEngine.Debug.Log(e.Message);
+        }
+    }
+}
